3ba49ad5943f1fc71a37dcea6ab91d03ac592d67
danilmhk <mine123@gmail.com>
2026-04-08 11:28:27 +0700

    вывод пользователей

--- blog/login/views.py
+++ blog/login/views.py
@@ -3,12 +3,16 @@ from .models import User
 from .forms import UserForm
 
 # Create your views here.
+
+def users(request):
+    users = User.objects.all()
+    return render(request, 'users.html', {'users': users})
 def add_user(request):
     if request.method == "POST":
         user = UserForm(request.POST)
         if user.is_valid():
             user.save()
-        return redirect('/')
+        return redirect('/users/')
     else:
         form = UserForm()
         return render(request, "add_user.html", {'form': form})
